f0ef5c4e4d553613ee8704f8b42c9f872929788e
Ernest Bright <brighte172@gmail.com>
2025-11-09 19:15:39 +0100

    Add inline styling to Header, UserProfile, MainContent, and Footer components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px',}}>
+            <p>© 2025 My Favorite Cities</p>
+        </footer>
+    );
+}
+
+export default Footer;
